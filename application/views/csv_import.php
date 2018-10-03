10fd271ef5c54473f436728633e4256a131c3e4e
javedwasim <javedafaq@gmail.com>
2018-10-03 09:02:21 -0700

    first commit

---
+++ application/views/csv_import.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <title>How to Import CSV Data into Mysql using Codeigniter</title>
+    
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+    <div>
+        <input type="button" value="print" onclick="PrintDiv();" />
+    </div>
+	<div class="container box" id="divToPrint">
+		<h3 align="center">How to Import CSV Data into Mysql using Codeigniter</h3>
+
+		<br />
+
+		<form method="post" id="import_csv" enctype="multipart/form-data">
+			<div class="form-group">
+				<label>Select CSV File</label>
+				<input type="file" name="csv_file" id="csv_file" required accept=".csv" />
+			</div>
+			<br />
+			<button type="submit" name="import_csv" class="btn btn-info" id="import_csv_btn">Import CSV</button>
+		</form>
+		<br />
+		<div id="imported_csv_data"></div>
+
+	</div>
+</body>
+</html>
+
+<script type="text/javascript">
+    function PrintDiv() {
+        var divToPrint = document.getElementById('divToPrint');
+        var popupWin = window.open('', '_blank', 'width=600,height=600');
+        popupWin.document.open();
+        popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
+        popupWin.document.close();
+    }
+</script>
+<script>
+$(document).ready(function(){
+
+	load_data();
+
+	function load_data()
+	{
+		$.ajax({
+			url:"<?php echo base_url(); ?>csv_import/load_data",
+			method:"POST",
+			success:function(data)
+			{
+				$('#imported_csv_data').html(data);
+			}
+		})
+	}
+	$('#import_csv').on('submit', function(event){
+		event.preventDefault();
+		$.ajax({
+			url:"<?php echo base_url(); ?>csv_import/import",
+			method:"POST",
+			data:new FormData(this),
+			contentType:false,
+			cache:false,
+			processData:false,
+			beforeSend:function(){
+				$('#import_csv_btn').html('Importing...');
+			},
+			success:function(data)
+			{
+				$('#import_csv')[0].reset();
+				$('#import_csv_btn').attr('disabled', false);
+				$('#import_csv_btn').html('Import Done');
+				load_data();
+			}
+		})
+	});
+	
+});
+</script>
